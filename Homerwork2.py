ff6d3378e47d1710e6ec32a85b97931fb5414ca0
rahimakhanim <rehimeagazade@gmail.com>
2023-10-31 12:09:14 +0400

    task-2

---
+++ Homerwork2.py
@@ -0,0 +1,67 @@
+# Task2
+# task1 :
+
+# tuple data tipine bir element elave et(bashqa data tipe cevirerek)
+
+
+# HELL
+# a = (1,2,3,4)
+# a = list(a)
+# a.append("2")
+# print(a)
+
+
+
+# task2:
+
+# bir dict var
+# proqram qur sorushsun Daxil edin 1 Keys 2 Values
+# 1 - e basilanda dictdeki keysleri yazdirsin
+# 2 - e basilanda dictdeki valueleri yazdirsin
+
+
+# HELL
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# a = int(input("Enter 1 or 2: "))
+
+# if a=="" or a!=1 or a!=2:
+#     print("Duzgun melumat daxil etmediniz")
+# elif a==1:
+#     print(thisdict.keys())
+# else:
+#     print(thisdict.values())
+
+
+
+# error mesaji bos olanda melumat daxil etmediniz
+
+# task3:
+
+# 2 set var :
+# yalniz ortaq melumatlari ekrana cixarin
+
+# HELL
+# thisset = {"apple", "banana", "cherry"}
+# thatset = {"apple", "orange", "pear", "cherry"}
+# for i in thisset:
+#     if i in thatset:
+#         print(i)
+
+# task4:
+# Dict icerisinde her birinin deyerleri int olan 5 keys values var
+# example : {"example":244}
+# HELL
+# thisDict = {
+#     "SagirdSayi" : 43534545,
+#     "MuellimSayi" : 323334,
+#     "LobarantSayi" : 1233232,
+#     "SinifSayi" : 38332,
+#     "KitabxanaSayi" : 483
+
+# }
---
+++ pythontask1.py
@@ -0,0 +1,102 @@
+# task 1
+
+# ad = input("Ad daxil edin: ")
+# if ad=="Aslan" or ad=="aslan":
+#     print("Aslan emi ogludur") 
+# elif ad=="Imran" or ad=="imran":
+#     print("Imran dayi ogludur")
+# elif ad=="Afaq" or ad == "afaq":
+#     print("Afaq bibi qizidir")
+# elif ad == "Uzeyir" or ad=="uzeyir":
+#     print("Uzeyir xala ogludur")
+# elif ad == "Shahin" or ad=="shahin":
+#     print("Shahin yaxin dostdur")
+# else:
+#     print(ad," kimdir? Men onu tanimadim")
+
+# task 2
+# login = "test"
+# parol = 12345
+# login2 = input()
+# parol2 = int(input())
+# if(login2!=login and parol==parol2):
+#     print("Login yanlishdir")
+# elif(login2==login and parol!=parol2):
+#     print("Parol yanlishdir")
+# elif(login2!=login and parol!=parol2):
+#     print("Login ve parol yanlishdir")
+# elif(login2=="" and (parol==parol2 or parol!=parol2)):
+#     print("Login daxil etmediniz")
+# elif(praol2=="" and (login==login2 or login!=login2)):
+#     print("Parol daxil etmediniz")
+# else:
+#     print("Login ve parol daxil etmediniz")
+
+# task3
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a>b and a>c:
+#     print(a)
+# elif b>a and b>c:
+#     print(b)
+# else:
+#     print(c)
+
+	
+
+
+# task4
+
+# boy = float(input("Boyununuz daxil edin:"))
+# kutle = float(input("Kutlenizi daxil edin:"))
+# bmi = kutle/(boy*boy)
+# if bmi<18.5:
+#     print("Zəif")
+
+# elif 18.5<bmi<25:
+#     print("Normal")
+# elif 20<bmi<30:
+#     print("kiloli")
+# elif 30<bmi:
+#     print("Obez")
+
+
+# task5
+# satiram = input()
+# aliram = input()
+# price = int(input())
+# if (satiram=='AZN' and aliram == 'USD'):
+#     print(price/1.7)
+# elif(satiram=='USD' and aliram == 'AZN'):
+#     print(price*1.7)
+# elif(satiram=='AZN' and aliram == 'EUR'):
+#     print(price*1.9)
+# elif(satiram=='EUR' and aliram == 'AZN'):
+#     print(price/1.9)
+
+
+
+
+# task6
+# test_list = [5, 6, [], 3, [], [], 9]
+# newList = []
+# for i in range(len(test_list)):
+#     if(type(test_list[i])== int):
+#         newList.append(test_list[i])
+
+# print(newList)
+
+
+
+# task7
+# adlar=[]
+# ad = input()
+# if ad =="":
+#     print("Ad daxil etmediniz")
+# elif len(ad)>15:
+#     print("Ad cox uzundur maksimum 15 herf")
+# else:
+#     adlar.append(ad)
+#     print("Ad Bazaya ugurla elave olundu ")
+
